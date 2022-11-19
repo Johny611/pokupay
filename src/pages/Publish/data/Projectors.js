713ea31c9a7b47ebe52ccc75b1ee44a1518638e6
Johny611 <mendelson611@gmail.com>
2022-11-19 17:49:42 +0500

    more changes

--- src/pages/Publish/data/Projectors.js
+++ src/pages/Publish/data/Projectors.js
@@ -2,17 +2,67 @@ import React from "react";
 import Options from "../components/Options";
 
 const types = [
-  { name: "", rus: "Модули памяти", eng: "", uzb: "" },
-  { name: "", rus: "Видеокарты", eng: "", uzb: "" },
-  { name: "", rus: "Звуковые карты", eng: "", uzb: "" },
-  { name: "", rus: "Материнские платы", eng: "", uzb: "" },
-  { name: "", rus: "Оптические приводы", eng: "", uzb: "" },
-  { name: "", rus: "Жесткие диски", eng: "", uzb: "" },
-  { name: "", rus: "Корпуса", eng: "", uzb: "" },
-  { name: "", rus: "Процессоры", eng: "", uzb: "" },
-  { name: "", rus: "ТВ-тюнеры", eng: "", uzb: "" },
-  { name: "", rus: "Блоки питания", eng: "", uzb: "" },
-  { name: "", rus: "Прочие комплектующие", eng: "", uzb: "" },
+  {
+    name: "memoryModules",
+    rus: "Модули памяти",
+    eng: "Memory modules",
+    uzb: "Xotira modullari",
+  },
+  {
+    name: "videoCards",
+    rus: "Видеокарты",
+    eng: "Video cards",
+    uzb: "Video kartalar",
+  },
+  {
+    name: "soundCards",
+    rus: "Звуковые карты",
+    eng: "Sound cards",
+    uzb: "Ovoz kartalari",
+  },
+  {
+    name: "motherboards",
+    rus: "Материнские платы",
+    eng: "Motherboards",
+    uzb: "Platalar",
+  },
+  {
+    name: "ppticalDrives",
+    rus: "Оптические приводы",
+    eng: "Optical drives",
+    uzb: "Optik drayvlar",
+  },
+  {
+    name: "hardDrives",
+    rus: "Жесткие диски",
+    eng: "Hard drives",
+    uzb: "Qattiq disklar",
+  },
+  { name: "frame", rus: "Корпуса", eng: "Frame", uzb: "Korpus" },
+  {
+    name: "processors",
+    rus: "Процессоры",
+    eng: "Processors",
+    uzb: "Protsessorlar",
+  },
+  {
+    name: "TVTuners",
+    rus: "ТВ-тюнеры",
+    eng: "TV tuners",
+    uzb: "Televizor tyunerlari",
+  },
+  {
+    name: "powerSupplies",
+    rus: "Блоки питания",
+    eng: "Power supplies",
+    uzb: "Quvvat manbalari",
+  },
+  {
+    name: "otherAccessories",
+    rus: "Прочие комплектующие",
+    eng: "Other accessories",
+    uzb: "Boshqa aksessuarlar",
+  },
 ];
 
 const Projectors = () => {
