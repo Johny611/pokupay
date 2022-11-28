c10d89781589a48e133255ad525a754b3cce9141
Johny611 <mendelson611@gmail.com>
2022-11-28 13:17:51 +0500

    more changes

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Product = () => {
   const [product, setProduct] = useState({});
@@ -29,7 +32,12 @@ const Product = () => {
   }, []);
   console.log(product);
 
-  return <div>Product</div>;
+  return (
+    <div className="flex-1 grid grid-cols-12 grid-rows-1 gap-4 place-items-stretch">
+      <div className="col-span-8">Main</div>
+      <div className="col-span-4">Side</div>
+    </div>
+  );
 };
 
 export default Product;
